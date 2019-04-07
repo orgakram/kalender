ec918f294486e227eb855fa1c11f12ae2e7b0bfc
Andreas <andreas.heix@sap.com>
2019-04-07 21:15:29 +0200

    Auswaerts noch ohne Turnier

--- hockey-date-reader.py
+++ hockey-date-reader.py
@@ -5,11 +5,7 @@ import collections
 import os
 from shutil import copyfile
 
-league_url = 'http://www.hbw-hockey.de/VVI-web/Ergebnisdienst/HED-XML.asp?XML=J&saison=FELD19&liga=BAW-JP-MA'
-#             http://www.hbw-hockey.de/VVI-web/Ergebnisdienst/HED-XML.asp?XML=J&saison=FELD19&liga=BAW-JP-WJB
-#             http://www.hbw-hockey.de/VVI-web/Ergebnisdienst/HED-XML.asp?XML=J&saison=FELD19&liga=BAW-JP-MB
-#             http://www.hbw-hockey.de/VVI-web/Ergebnisdienst/HED-XML.asp?XML=J&saison=FELD19&liga=BAW-JP-MJB
-#             http://www.hbw-hockey.de/VVI-web/Ergebnisdienst/HED-XML.asp?XML=J&saison=FELD19&liga=BAW-JP-KB
+#league_url = 'http://www.hbw-hockey.de/VVI-web/Ergebnisdienst/HED-XML.asp?XML=J&saison=FELD19&liga=BAW-JP-MA'
 
 url_template = 'http://www.hbw-hockey.de/VVI-web/Ergebnisdienst/HED-XML.asp?XML=J&saison=FELD19&liga=BAW-{}'
 
@@ -30,33 +26,46 @@ away_games_file = "auswaertsspiele.csv"
 
 
 our_team = 'Karlsruher TV'
-home_games = True
+home_games_flag = False
 headers = 'Subject, Start Date, Start Time, End Date, End Time, All Day Event, Description, Location'
 subject = 'MA VL'
 all_day_event = 'False'
-description_template = 'Andreas Anpfiff {} - Spiel {}'
+home_game_description_template = 'Andreas Anpfiff {} - Spiel {}'
+away_game_description_template = 'Andreas Anpfiff {} - Spiel {}'
 location = 'Platzanlage'
 
-def parse_calendar(team_name, file_name, league_name):
+def parse_calendar(team_name, file_name, league_name, home_games_flag):
     calendar_xml = untangle.parse(file_name)
     entries=[]
     for gruppe in calendar_xml.Liga.Gruppe:
         for spiel in gruppe.Spiel:
-            hosting_team_name = str(spiel.STEA.cdata)
+            visitor = spiel.STEG.cdata
+            if (home_games_flag):
+                hosting_team_name = str(spiel.STEA.cdata)
+            else:
+                hosting_team_name = str(spiel.STEG.cdata)
+                visitor = spiel.STEA.cdata
+
             if (hosting_team_name == our_team):
                 date = spiel.SDAG.cdata
-                visitor = spiel.STEG.cdata
                 kickoff_time = spiel.SUHR.cdata
                 game_id = spiel.SNAM.cdata
-                description = description_template.format(kickoff_time, game_id)
-                reservation_times = calculate_start_and_end_of_reservation(kickoff_time, league_name)
-                line_entry = f"{team_name} - Anpfiff {kickoff_time} - Gast {visitor}, {date}, {reservation_times.start}, {date}, {reservation_times.end}, {all_day_event}, {description}, {location}\n"
+
+                if (home_games_flag):
+                    description = home_game_description_template.format(kickoff_time, game_id)
+                    reservation_times = calculate_start_and_end_of_reservation(kickoff_time, league_name)
+                    line_entry = f"{team_name} - Anpfiff {kickoff_time} - Gast {visitor}, {date}, {reservation_times.start}, {date}, {reservation_times.end}, {all_day_event}, {description}, {location}\n"
+                else:
+                    description = home_game_description_template.format(kickoff_time, game_id)
+                    reservation_times = calculate_start_and_end_of_reservation(kickoff_time, league_name)
+                    line_entry = f"{team_name} - Anpfiff {kickoff_time} - beim {visitor}, {date}, {kickoff_time}, {date}, {reservation_times.end}, {all_day_event}, {description}, {visitor}\n"
+
                 entries.append(line_entry)
                 print(line_entry)
 
     return entries
 
-def retrieve_league_data(leagues):
+def retrieve_league_data(leagues, home_games_flag):
     calendar_lines=[]
     for league in leagues:
         league_url = url_template.format(league)
@@ -69,19 +78,24 @@ def retrieve_league_data(leagues):
         if (league in tournaments):
             print(f"{league} hat Turnierbetrieb!")
         else:
-            new_entries = parse_calendar(team_name, file_name, league)
+            new_entries = parse_calendar(team_name, file_name, league, home_games_flag)
             calendar_lines = calendar_lines + new_entries
 
+    if (home_games_flag):
+        current_games_file = home_games_file
+    else:
+        current_games_file = away_games_file
+
     # copy away old status so we can compare
-    old_file_name = "old_" + home_games_file
+    old_file_name = "old_" + current_games_file
     old_entries=[]
-    if (os.path.isfile(home_games_file)):
-        copyfile(home_games_file, old_file_name)
-        with open(home_games_file) as f:
+    if (os.path.isfile(current_games_file)):
+        copyfile(current_games_file, old_file_name)
+        with open(current_games_file) as f:
                 old_entries = f.readlines()
 
     # write the new status.
-    with open(home_games_file, "w") as text_file:
+    with open(current_games_file, "w") as text_file:
         for line in calendar_lines:
             text_file.write(line)
 
@@ -94,7 +108,7 @@ def retrieve_league_data(leagues):
         new_entries = new_set - result
         print("******** neue Einträge")
         for entry in new_entries:
-            print(new_entries)
+            print(entry)
         print("******** neue Einträge")
 
 
@@ -111,5 +125,5 @@ def calculate_start_and_end_of_reservation(time_kickoff : str, league : str):
     return calendar_start_end(start=start.time(), end=end.time())
 
 
-retrieve_league_data(leagues)
+retrieve_league_data(leagues, home_games_flag)
 #calculate_start_and_end_of_reservation("10:00", "JP-MA")
